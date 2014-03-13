2fb15cedb0386ae369d0f9b87e69d43d2d650b7a
ionpan <ionpan@gmail.com>
2014-03-13 02:54:09 +0200

    Changes on comments

--- example/example.ino
+++ example/example.ino
@@ -2,7 +2,7 @@
  7-17-2011
  Spark Fun Electronics 2011
  Nathan Seidle
-Modified on 03-12-2014 by ionpan
+ Modified on 03-12-2014 by ionpan
  
  This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
  
